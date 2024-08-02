952852d1e253a2895f135fe2c0d32831104e27b3
Mauricio Godoy <mgodoy@stacktrace.com.ar>
2024-08-01 21:36:56 -0300

    feat: inicializar aplicación

---
+++ src/test/java/mgodoy/pokedex/PokedexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mgodoy.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
